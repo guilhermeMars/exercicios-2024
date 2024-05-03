4f0118c6907ba8aaade12b8b5fc3304c4e09e767
guilhermeMars <guilhermemspiandorin@gmail.com>
2024-05-03 15:24:53 -0300

    Projeto finalizado!
    Todas as funcionalidades foram adicionadas conforme requisitado no figma do projeto

--- ts/src/app/app.component.ts
+++ ts/src/app/app.component.ts
@@ -7,4 +7,56 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'DevChuva';
+  createTopicDisplay: string = 'flex';
+  createTopicFormDisplay: string = 'none';
+  sendedPopupDisplay: string = 'none';
+
+  // constructor(private elementRef: ElementRef){}
+
+  // ngAfterViewInit(): void {
+  //   this.addAnswerListener();
+  // }
+
+  // addAnswerListener(): void{
+  //   const answerElement = this.elementRef.nativeElement.querySelectorAll(".answered-topic");
+  //   if(answerElement){
+  //     answerElement.addEventListener('click', this.expandAnswer);
+  //   }
+  // }
+
+  expandAnswer(event: MouseEvent): void{
+    const element = event.currentTarget as HTMLElement;
+    if (String(element.classList).includes("truncated-comment")) {
+      element.classList.remove("truncated-comment");
+    } else{
+      element.classList.add("truncated-comment");
+    }
+  }
+
+  createTopic(): void{
+    this.createTopicDisplay = 'none';
+    this.createTopicFormDisplay = 'flex';
+    this.sendedPopupDisplay = 'none';
+  }
+
+  sendTopic(): void{
+    this.createTopicDisplay = 'none';
+    this.createTopicFormDisplay = 'none';
+    this.sendedPopupDisplay = 'block';
+  }
+
+  createAnotherTopic(): void{
+    this.createTopicDisplay = 'none';
+    this.createTopicFormDisplay = 'flex';
+    this.sendedPopupDisplay = 'none';
+  }
+
+  expandItem(event: MouseEvent): void {
+    const element = event.currentTarget as HTMLElement;
+    if (String(element.classList).includes("truncated")) {
+      element.classList.remove("truncated");
+    } else{
+      element.classList.add("truncated");
+    }
+  }
 }
